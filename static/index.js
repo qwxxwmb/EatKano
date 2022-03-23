2054f9ed93f83f92be0a3e1831a3118ad18bb2db
qwxxwmb <102198675+qwxxwmb@users.noreply.github.com>
2022-03-23 12:42:02 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -465,10 +465,10 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
+        if (cps <= 5) return '你个d人';
+        if (cps <= 8) return '我在做正确的事';
+        if (cps <= 10)  return '但是没人能理解我';
+        if (cps <= 50) return '我很久没洗头了';
         return '人？';
     }
 
